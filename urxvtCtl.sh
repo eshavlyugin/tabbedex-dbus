82422dd5312c70643437c0790f381474330a2f74
Evgeny Shavlyugin <eshavlyugin@gmail.com>
2015-09-06 23:34:41 +0300

    Introduced urxvtCtl.sh script. Few bugs fixed.
    urxvtCtl.sh to simplify tabbed urxvt window control. Based on dbus-send utility

---
+++ urxvtCtl.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+for i in "$@"
+do
+    case $i in
+	-t=*|--new-tab=*)
+	    tab_name="${i#*=}"
+	    shift # past argument=value
+	    ;;
+	-a=*|--activate-tab=*)
+	    tab_active_name="${i#*=}"
+	    shift # past argument=value
+	    ;;
+	-e=*|--exec=*)
+	    exec_cmd_line="${i#*=}"
+	    shift # past argument=value
+	    ;;
+        -f|--foreground)
+	    foreground="1"
+	    shift # past argument with no value
+	    ;;
+	*)
+	    # unknown option
+	    ;;
+    esac
+done
+
+echo "${tab_name}"
+echo "${tab_active_name}"
+echo "${exec_cmd_line}"
+
+winid_hex=`xwininfo -root -children | grep urxvt | awk '{ print $1; }' | head -n1`
+echo ${winid_hex}
+winid_dec=`printf "%d\n" ${winid_hex}`
+echo ${winid_dec}
+
+cmd_line="dbus-send --session --print-reply --dest=org.schmorp.urxvt /term/${winid_dec}/control "
+
+if [ -n "${tab_name}" ]; then
+    eval "${cmd_line} org.schmorp.urxvt.addTabWithName string:\"${tab_name}\""
+fi
+if [ -n "${tab_active_name}" ]; then
+    eval "${cmd_line} org.schmorp.urxvt.addOrActivateTabByName string:\"${tab_active_name}\""
+fi
+if [ -n "${exec_cmd_line}" ]; then
+    eval "${cmd_line} org.schmorp.urxvt.execCmd string:\"${exec_cmd_line}\""
+fi
+if [ -n "${foreground}" ]; then
+    true
+fi
